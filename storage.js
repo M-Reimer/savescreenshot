64bbfcf8ed64ebd2b04bf8b56023638c29126429
Manuel Reimer <manuel.reimer@gmx.de>
2020-06-24 08:21:25 +0200

    Version 7.0.0

--- storage.js
+++ storage.js
@@ -1,6 +1,6 @@
 /*
     Firefox addon "Save Screenshot"
-    Copyright (C) 2019  Manuel Reimer <manuel.reimer@gmx.de>
+    Copyright (C) 2020  Manuel Reimer <manuel.reimer@gmx.de>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
